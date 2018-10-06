b9214ede568cee92684572ebac2bc2c13f9ffbe7
Aaron Lee <alee@aaronosaur.us>
2018-10-05 20:59:04 -0500

    point and vector factories

--- src/point.rs
+++ src/point.rs
@@ -14,6 +14,28 @@ impl Tuple {
     pub fn is_point(&self) -> bool {
         self.w == 1.0
     }
+
+    pub fn is_equal(&self, t: Tuple) -> bool {
+        (self.x == t.x) && (self.y == t.y) && (self.z == t.z) && (self.w == t.w)
+    }
+
+    pub fn point(x: f32, y: f32, z: f32) -> Tuple {
+        Tuple {
+            x: x,
+            y: y,
+            z: z,
+            w: 1.0,
+        }
+    }
+
+    pub fn vector(x: f32, y: f32, z: f32) -> Tuple {
+        Tuple {
+            x: x,
+            y: y,
+            z: z,
+            w: 0.0,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -50,5 +72,31 @@ mod tests {
         assert_eq!(t.w, 0.0);
         assert_eq!(t.is_point(), false);
         assert_eq!(t.is_vector(), true);
+    }
+
+    #[test]
+    fn point_factory_creates_point() {
+        let p = Tuple::point(4.0, -4.0, 3.0);
+        let t = Tuple {
+            x: 4.0,
+            y: -4.0,
+            z: 3.0,
+            w: 1.0,
+        };
+
+        assert_eq!(p.is_equal(t), true);
+    }
+
+    #[test]
+    fn vector_factory_creates_vector() {
+        let v = Tuple::vector(4.0, -4.0, 3.0);
+        let t = Tuple {
+            x: 4.0,
+            y: -4.0,
+            z: 3.0,
+            w: 0.0,
+        };
+
+        assert_eq!(v.is_equal(t), true);
     }
 }
