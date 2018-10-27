8b15f6324f2487d1099219aebeaa4771093cdbd9
Aaron Lee <alee@aaronosaur.us>
2018-10-27 10:03:57 -0500

    matrix can handle different sizes

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,11 +1,22 @@
 #[derive(Debug)]
 pub struct Matrix {
-    m: [[f32; 4]; 4],
+    m: Vec<Vec<f32>>,
 }
 
 impl Matrix {
-    pub fn new(m: [[f32; 4]; 4]) -> Matrix {
-        Matrix { m: m }
+    pub fn new(m: Vec<Vec<f32>>) -> Matrix {
+        let mut mat = Matrix { m: vec![vec![0.;4]; 4]};
+
+        let mut row_num = 0;
+        for row in m.iter() {
+            let mut col_num = 0;
+            for col in row.iter() {
+                mat.m[row_num][col_num] = *col;
+                col_num += 1;
+            }
+            row_num += 1;
+        }
+        mat
     }
 
     // for now this throws an index bounds panic if x,y is off the matrix
@@ -20,13 +31,41 @@ mod tests {
 
     #[test]
     fn new_4x4_matrix() {
-        let m = Matrix::new([
-            [1., 2., 3., 4.],
-            [5.5, 6.5, 7.5, 8.5],
-            [9., 10., 11., 12.],
-            [13.5, 14.5, 15.5, 16.5],
+        let m = Matrix::new(vec![
+            vec![1., 2., 3., 4.],
+            vec![5.5, 6.5, 7.5, 8.5],
+            vec![9., 10., 11., 12.],
+            vec![13.5, 14.5, 15.5, 16.5],
         ]);
 
         assert_eq!(m.get(0, 0), 1.);
+        assert_eq!(m.get(0,3), 4.);
+        assert_eq!(m.get(3,2), 15.5);
     }
+
+    #[test]
+    fn new_3x3_matrix() {
+        let m = Matrix::new(vec![
+            vec![1., 2., 3.],
+            vec![5.5, 6.5, 7.5],
+            vec![9., 10., 11.],
+        ]);
+
+        assert_eq!(m.get(0, 0), 1.);
+        assert_eq!(m.get(0,2), 3.);
+        assert_eq!(m.get(2,1), 10.);
+    }
+
+    #[test]
+    fn new_2x2_matrix() {
+        let m = Matrix::new(vec![
+            vec![1., 2.],
+            vec![5.5, 6.5],
+        ]);
+
+        assert_eq!(m.get(0, 0), 1.);
+        assert_eq!(m.get(0,1), 2.);
+        assert_eq!(m.get(1,1), 6.5);
+    }
+
 }
